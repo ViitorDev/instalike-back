fe74c7455de478f635e18e1ea61f1d0ce122b5a5
ViitorDev <vitordev7@gmail.com>
2024-11-21 22:52:48 -0300

    add

--- server.js
+++ server.js
@@ -8,3 +8,26 @@ app.listen(3000, () => {
 app.get("/api", (req, res) => {
   res.status(200).send("Boas vindas à imersâo!");
 });
+
+
+// exercicio
+app.get("/livro", (req, res) => {
+
+  // opção 1
+  /*const livro = {
+    titulo: "O Senhor dos Anéis",
+    autor: "J.R.R. Tolkien",
+    ano: 1954,
+    genero: "Fantasia"
+  }
+  res.status(200).send(livro)
+  */
+
+  // opção 2
+  res.status(200).send({
+    titulo: "O Senhor dos Anéis",
+    autor: "J.R.R. Tolkien",
+    ano: 1954,
+    genero: "Fantasia"
+  })
+})
